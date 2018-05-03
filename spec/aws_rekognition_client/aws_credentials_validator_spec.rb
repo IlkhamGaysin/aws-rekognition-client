f8424bc6a10594edc3fab2226d0aab69aaf9f042
IlkhamGaysin <ilgamgaysin@gmail.com>
2018-05-03 21:23:03 +0300

    Fix quality checks and add target ruby version for rubocop

--- spec/aws_rekognition_client/aws_credentials_validator_spec.rb
+++ spec/aws_rekognition_client/aws_credentials_validator_spec.rb
@@ -3,32 +3,34 @@
 require 'spec_helper'
 
 RSpec.describe AwsRekognitionClient::AwsCredentialsValidator do
-  context '.validate!' do
+  describe '.validate!' do
     context 'when access key is blank' do
-      let(:instance) { instance_double(AwsRekognitionClient::Configuration, access_key: nil, secret_key: Faker::Crypto.sha1) }
+      let(:instance) do
+        instance_double(AwsRekognitionClient::Configuration, access_key: nil, secret_key: Faker::Crypto.sha1)
+      end
 
       before do
         allow(AwsRekognitionClient::Configuration).to receive(:instance).and_return(instance)
       end
 
       it 'raises BaseError' do
-        expect {
-          described_class.validate!
-        }.to raise_error(AwsRekognitionClient::BaseError, 'AWS Access Key must be present')
+        expect { described_class.validate! }
+          .to raise_error(AwsRekognitionClient::BaseError, 'AWS Access Key must be present')
       end
     end
 
     context 'when secret key is blank' do
-      let(:instance) { instance_double(AwsRekognitionClient::Configuration, secret_key: nil, access_key: Faker::Crypto.sha1) }
+      let(:instance) do
+        instance_double(AwsRekognitionClient::Configuration, secret_key: nil, access_key: Faker::Crypto.sha1)
+      end
 
       before do
         allow(AwsRekognitionClient::Configuration).to receive(:instance).and_return(instance)
       end
 
       it 'raises BaseError' do
-        expect {
-          described_class.validate!
-        }.to raise_error(AwsRekognitionClient::BaseError, 'AWS Secret Key must be present')
+        expect { described_class.validate! }
+          .to raise_error(AwsRekognitionClient::BaseError, 'AWS Secret Key must be present')
       end
     end
 
@@ -45,9 +47,8 @@ RSpec.describe AwsRekognitionClient::AwsCredentialsValidator do
       end
 
       it 'raises BaseError' do
-        expect {
-          described_class.validate!
-        }.to raise_error(AwsRekognitionClient::BaseError, 'AWS region must be present')
+        expect { described_class.validate! }
+          .to raise_error(AwsRekognitionClient::BaseError, 'AWS region must be present')
       end
     end
   end
