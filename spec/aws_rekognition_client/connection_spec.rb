e88b60e78233c558c2747617d7e7b58b045722c1
IlkhamGaysin <ilgamgaysin@gmail.com>
2018-05-04 11:59:54 +0300

    Fix quality issues

--- spec/aws_rekognition_client/connection_spec.rb
+++ spec/aws_rekognition_client/connection_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 
 RSpec.describe AwsRekognitionClient::Connection do
   let(:status)  { 200 }
-  let(:headers) { Hash.new }
+  let(:headers) { {} }
   let(:body)    { { 'Labels': [] } }
   let(:url)     { 'https://rekognition.eu-west-1.amazonaws.com' }
 
@@ -22,11 +22,9 @@ RSpec.describe AwsRekognitionClient::Connection do
     allow(Faraday).to receive(:new).and_return(http_stubs)
   end
 
-  it "returns status, headers, body" do
+  it 'returns status, headers, body' do
     result = connection.post(url, body, headers)
 
-    expect(result.status).to eq(status)
-    expect(result.headers).to eq(headers)
-    expect(result.body).to eq(body)
+    expect(result). to have_attributes(status: status, headers: headers, body: body)
   end
 end
